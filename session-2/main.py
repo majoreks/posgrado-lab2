5e392ca35ca82da68402fbd02559d43a4745f57b
majoreks <s.majorek.konta@gmail.com>
2025-03-05 00:55:36 +0100

    full training loop

--- session-2/main.py
+++ session-2/main.py
@@ -5,16 +5,17 @@ import torch.optim as optim
 import torch.nn.functional as F
 from dataset import MyDataset
 from model import MyModel
-from utils import accuracy, save_model
+from utils import AverageMeter, accuracy, save_model
 from transforms import data_transforms
 import os
 import argparse
 from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data-dir", help="path to data dir", type=str, default='data/')
 parser.add_argument("--info-fname", help="file name of csv with data info", type=str, default='chinese_mnist.csv')
-parser.add_argument("--n-epochs", help="number of epochs to train", type=int, default=10)
+parser.add_argument("--n-epochs", help="number of epochs to train", type=int, default=5)
 parser.add_argument("--batch-size", help="batch size", type=int, default=100)
 parser.add_argument("--lr", help="learning rate", type=float, default=0.1)
 args = parser.parse_args()
@@ -22,17 +23,31 @@ args = parser.parse_args()
 device = torch.device(
     "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-def train_single_epoch(model, criterion, optimizer, dataloader):
+def train_single_epoch(model, criterion, optimizer, dataloader, train_loss, train_acc):
+    train_loss.reset()
+    train_acc.reset()
     for x, y in tqdm(dataloader):
         x, y = x.to(device), y.to(device)
-        outputs = model(x)
-        loss = criterion(outputs, y)
+        optimizer.zero_grad()
+        y_ = model(x)
+        loss = criterion(y_, y)
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
 
-# def eval_single_epoch(...):
-#     pass
+        train_loss.update(loss.item(), n=len(y))
+        train_acc.update(accuracy(y, y_), n=len(y))
+
+def eval_single_epoch(model, criterion, dataloader, val_loss, val_acc):
+    val_loss.reset()
+    val_acc.reset()
+    for x, y in tqdm(dataloader):
+        x, y = x.to(device), y.to(device)
+        y_ = model(x)
+        loss = criterion(y_, y)
+        loss.backward()
+
+        val_loss.update(loss.item(), n=len(y))
+        val_acc.update(accuracy(y, y_), n=len(y))
 
 
 def train_model(config):
@@ -44,11 +59,36 @@ def train_model(config):
     criterion = F.nll_loss
     optimizer = optim.Adam(my_model.parameters(), lr=config['lr'])
     
-    x, y = next(iter(dataloader))
-    for epoch in tqdm(range(config["epochs"]), 'epoch'):
-        train_single_epoch(my_model, criterion, optimizer, dataloader)
-    #     train_single_epoch(...)
-    #     eval_single_epoch(...)
+    train_accuracies, train_losses, val_accuracies, val_losses = [], [], [], []
+    val_loss = AverageMeter()
+    val_accuracy = AverageMeter()
+    train_loss = AverageMeter()
+    train_accuracy = AverageMeter()
+
+    for _ in tqdm(range(config["epochs"]), 'epoch'):
+        my_model.train()
+        train_single_epoch(my_model, criterion, optimizer, dataloader, train_loss, train_accuracy)
+        train_losses.append(train_loss.avg)
+        train_accuracies.append(train_accuracy.avg)
+        
+        my_model.eval()
+        eval_single_epoch(my_model, criterion, dataloader, val_loss, val_accuracy)
+        val_losses.append(val_loss.avg)
+        val_accuracies.append(val_accuracy.avg)
+   
+    epochs = range(len(train_accuracies))
+
+    plt.plot(epochs, train_accuracies, 'b', label='Training acc')
+    plt.plot(epochs, val_accuracies, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(os.path.dirname(__file__), 'acc.png'), dpi=400)       
+
+    plt.plot(epochs, train_losses, 'b', label='Training loss')
+    plt.plot(epochs, val_losses, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.savefig(os.path.join(os.path.dirname(__file__), 'loss.png'), dpi=400)
 
     return my_model
 
--- session-2/utils.py
+++ session-2/utils.py
@@ -3,9 +3,26 @@ import torch
 
 def accuracy(labels, outputs):
     preds = outputs.argmax(-1)
-    acc = (preds == labels.view_as(preds)).float().detach().numpy().mean()
+    acc = (preds == labels.view_as(preds)).cpu().float().detach().numpy().mean()
     return acc
     
 
 def save_model(model, path):
     torch.save(model.state_dict(), path)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
