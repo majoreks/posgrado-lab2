73448d3a688bbdd86a0d68213ddb23c492cf4753
majoreks <s.majorek.konta@gmail.com>
2025-03-08 18:11:47 +0100

    cleanup

--- session-2/utils.py
+++ session-2/utils.py
@@ -9,20 +9,3 @@ def accuracy(labels, outputs):
 
 def save_model(model, path):
     torch.save(model.state_dict(), path)
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
