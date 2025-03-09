0dfe7e4c53af53788e12decc8d83869489216603
majoreks <s.majorek.konta@gmail.com>
2025-03-09 21:37:22 +0100

    refactor

--- session-2/main.py
+++ session-2/main.py
@@ -3,20 +3,14 @@ import sklearn.model_selection
 import torch
 
 from torch.utils.data import DataLoader
-import torch.optim as optim
-import torch.nn as nn
 from dataset import MyDataset
-from model import MyModel
 from transforms import data_transforms
 import os
 import argparse
-from tqdm.auto import tqdm
-import matplotlib.pyplot as plt
 import sklearn
 import numpy as np
 import random
-from training import train_single_epoch, eval_single_epoch
-from device import device
+from training import train_model
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data-dir", help="path to data dir", type=str, default='data/')
@@ -34,45 +28,6 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 set_seed(42)
-
-def train_model(config, train_dataset, val_dataset):
-    my_model = MyModel(config['mlp_width']).to(device)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=8) 
-    valid_dataloader = DataLoader(val_dataset, batch_size=config['batch_size']) 
-
-    criterion = nn.NLLLoss()
-    optimizer = optim.Adam(my_model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
-    
-    train_accuracies, train_losses, val_accuracies, val_losses = [], [], [], []
-
-    for _ in tqdm(range(config["epochs"]), 'epoch'):
-        train_epoch_loss, train_epoch_acc = train_single_epoch(my_model, criterion, optimizer, train_dataloader)
-        
-        train_losses.append(np.asarray(train_epoch_loss).mean())
-        train_accuracies.append(np.asarray(train_epoch_acc).mean())
-        
-        val_epoch_loss, val_epoch_acc = eval_single_epoch(my_model, criterion, valid_dataloader)
-        val_losses.append(np.asarray(val_epoch_loss).mean())
-        val_accuracies.append(np.asarray(val_epoch_acc).mean())
-   
-    stats_len = range(len(train_accuracies))
-
-    plt.plot(stats_len, train_accuracies, 'b', label='Training acc')
-    plt.plot(stats_len, val_accuracies, 'r', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.savefig(os.path.join(os.path.dirname(__file__), 'acc.png'), dpi=400)       
-
-    plt.figure()
-    plt.plot(stats_len, train_losses, 'b', label='Training loss')
-    plt.plot(stats_len, val_losses, 'r', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-    plt.savefig(os.path.join(os.path.dirname(__file__), 'loss.png'), dpi=400)
-
-    return my_model
-
 
 if __name__ == "__main__":
 
--- session-2/training.py
+++ session-2/training.py
@@ -1,7 +1,14 @@
+import os
+from matplotlib import pyplot as plt
+import numpy as np
 from torch import device
 from tqdm import tqdm
 from utils import accuracy
 from device import device
+from model import MyModel
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn as nn
 
 def train_single_epoch(model, criterion, optimizer, dataloader):
     epoch_loss = []
@@ -35,3 +42,41 @@ def eval_single_epoch(model, criterion, dataloader):
         epoch_acc.append(accuracy(y, y_))
     
     return epoch_loss, epoch_acc
+
+def train_model(config, train_dataset, val_dataset):
+    my_model = MyModel(config['mlp_width']).to(device)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=8) 
+    valid_dataloader = DataLoader(val_dataset, batch_size=config['batch_size']) 
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(my_model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+    
+    train_accuracies, train_losses, val_accuracies, val_losses = [], [], [], []
+
+    for _ in tqdm(range(config["epochs"]), 'epoch'):
+        train_epoch_loss, train_epoch_acc = train_single_epoch(my_model, criterion, optimizer, train_dataloader)
+        
+        train_losses.append(np.asarray(train_epoch_loss).mean())
+        train_accuracies.append(np.asarray(train_epoch_acc).mean())
+        
+        val_epoch_loss, val_epoch_acc = eval_single_epoch(my_model, criterion, valid_dataloader)
+        val_losses.append(np.asarray(val_epoch_loss).mean())
+        val_accuracies.append(np.asarray(val_epoch_acc).mean())
+   
+    stats_len = range(len(train_accuracies))
+
+    plt.plot(stats_len, train_accuracies, 'b', label='Training acc')
+    plt.plot(stats_len, val_accuracies, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.savefig(os.path.join(os.path.dirname(__file__), 'acc.png'), dpi=400)       
+
+    plt.figure()
+    plt.plot(stats_len, train_losses, 'b', label='Training loss')
+    plt.plot(stats_len, val_losses, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.savefig(os.path.join(os.path.dirname(__file__), 'loss.png'), dpi=400)
+
+    return my_model
